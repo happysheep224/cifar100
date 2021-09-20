f0861b12403ae9eca94043b54e1afd6cb5ff5a50
happysheep224 <89255775@qq.com>
2021-09-20 19:19:53 +0800

    4th

--- Test5_resnet/train_c10.py
+++ Test5_resnet/train_c10.py
@@ -77,7 +77,7 @@ def main():
     params = [p for p in net.parameters() if p.requires_grad]
     optimizer = optim.Adam(params, lr=0.0001)
 
-    epochs = 3
+    epochs = 200
     best_acc = 0.0
     save_path = './resNet34.pth'
     train_steps = len(train_loader)
