b5d75a3371560f3cd9bc7c2909fa57596f45f819
happysheep224 <89255775@qq.com>
2021-09-27 18:31:44 +0800

    download resnet checkpoint

--- Test5_resnet/train_c10.py
+++ Test5_resnet/train_c10.py
@@ -6,8 +6,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms, datasets
 from tqdm import tqdm
+import torchvision.models as models
 
-from model import resnet34
+from model import resnet50
 
 
 def main():
@@ -55,8 +56,10 @@ def main():
         print("Shape of X [N, C, H, W]: ", X.shape)
         print("Shape of y: ", y.shape, y.dtype)
         break
-    net = resnet34()
-   # net.cuda()
+
+    # model_weight_path = "./resnet34-pre.pth"
+    net = models.resnet18(pretrained=True)
+    # net.cuda()
     # # load pretrain weights
     # # download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
     # model_weight_path = "./resnet34-pre.pth"
@@ -68,64 +71,65 @@ def main():
     # # change fc layer structure
     # in_channel = net.fc.in_features
     # net.fc = nn.Linear(in_channel, 100)
-    net.to(device)
-
-    # define loss function
-    loss_function = nn.CrossEntropyLoss()
-
-    # construct an optimizer
-    params = [p for p in net.parameters() if p.requires_grad]
-    optimizer = optim.Adam(params, lr=0.0001)
-
-    epochs = 200
-    best_acc = 0.0
-    save_path = './resNet34.pth'
-    train_steps = len(train_loader)
-    for epoch in range(epochs):
-        # train
-        net.train()
-        running_loss = 0.0
-        train_bar = tqdm(train_loader)
-        for step, data in enumerate(train_bar):
-            images, labels = data
-            optimizer.zero_grad()
-            logits = net(images.to(device))
-            loss = loss_function(logits, labels.to(device))
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
-        # validate
-        net.eval()
-        acc = 0.0  # accumulate accurate number / epoch
-        with torch.no_grad():
-            val_bar = tqdm(test_loader)
-            for val_data in val_bar:
-                val_images, val_labels = val_data
-                outputs = net(val_images.to(device))
-                # loss = loss_function(outputs, test_labels)
-                predict_y = torch.max(outputs, dim=1)[1]
-                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
-
-                val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
-                                                           epochs)
-
-        val_accurate = acc / len_test
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
-              (epoch + 1, running_loss / train_steps, val_accurate))
-
-        if val_accurate > best_acc:
-            best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
-
-    print('Finished Training')
-
+    # net.to(device)#move to device
+    # print(net)
+    #
+    # # define loss function
+    # loss_function = nn.CrossEntropyLoss()
+    #
+    # # construct an optimizer
+    # params = [p for p in net.parameters() if p.requires_grad]
+    # optimizer = optim.Adam(params, lr=0.0001)
+    #
+    # epochs = 200
+    # best_acc = 0.0
+    # save_path = './resNet34.pth'
+    # train_steps = len(train_loader)
+    # for epoch in range(epochs):
+    #     # train
+    #     net.train()
+    #     running_loss = 0.0
+    #     train_bar = tqdm(train_loader)
+    #     for step, data in enumerate(train_bar):
+    #         images, labels = data
+    #         optimizer.zero_grad()
+    #         logits = net(images.to(device))
+    #         loss = loss_function(logits, labels.to(device))
+    #         loss.backward()
+    #         optimizer.step()
+    #
+    #         # print statistics
+    #         running_loss += loss.item()
+    #
+    #         train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+    #                                                                  epochs,
+    #                                                                  loss)
+    #
+    #     # validate
+    #     net.eval()
+    #     acc = 0.0  # accumulate accurate number / epoch
+    #     with torch.no_grad():
+    #         val_bar = tqdm(test_loader)
+    #         for val_data in val_bar:
+    #             val_images, val_labels = val_data
+    #             outputs = net(val_images.to(device))
+    #             # loss = loss_function(outputs, test_labels)
+    #             predict_y = torch.max(outputs, dim=1)[1]
+    #             acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+    #
+    #             val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+    #                                                        epochs)
+    #
+    #     val_accurate = acc / len_test
+    #     print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+    #           (epoch + 1, running_loss / train_steps, val_accurate))
+    #
+    #     if val_accurate > best_acc:
+    #         best_acc = val_accurate
+    #         torch.save(net.state_dict(), save_path)
+    #
+    # print('Finished Training')
+    #
 
 if __name__ == '__main__':
     main()
