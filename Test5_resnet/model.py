1b528c669d24b4836c20b87d29a0e415071d2af1
Yang Yang <89255775@qq.com>
2021-10-06 14:30:19 +0800

    model

--- Test5_resnet/model.py
+++ Test5_resnet/model.py
@@ -90,8 +90,8 @@ class ResNet(nn.Module):
     def __init__(self,
                  block,
                  blocks_num,
-                 inputchannel=3,
-                 num_classes=100,
+                 inputchannel,
+                 num_classes,
                  include_top=True,
                  groups=1,
                  width_per_group=64):
@@ -162,9 +162,9 @@ class ResNet(nn.Module):
         return x
 
 
-def resnet34(num_classes=1000, inputchannel = 3, include_top=True):
+def resnet34(num_classes, inputchannel = 728, include_top=True):
     # https://download.pytorch.org/models/resnet34-333f7ec4.pth
-    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, inputchannel=3,include_top=include_top)
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, inputchannel,include_top=include_top)
 
 
 def resnet50(num_classes=1000, include_top=True):
