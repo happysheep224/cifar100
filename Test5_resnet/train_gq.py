1883c53585e3df1fa10dd66e3ccbf9c36b108bf7
happysheep224 <89255775@qq.com>
2021-11-08 14:08:57 +0800

    linux

--- Test5_resnet/train_gq.py
+++ Test5_resnet/train_gq.py
@@ -12,6 +12,7 @@ from torch.utils.data import DataLoader, Dataset
 from model import resnet34,resnet50,resnet101
 from scipy.io import loadmat, savemat
 
+class_name_dict = {"GS": 0, "MG": 1, "NX": 2, "QH": 3, "XJ": 4}
 
 class GOU_QI_DATA(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -23,39 +24,47 @@ class GOU_QI_DATA(Dataset):
         return len(self.images)
 
     def __getitem__(self, index):
+        
         image_index = self.images[index]
         img_path = os.path.join(self.root_dir, image_index)
-        img = loadmat(img_path)["B"]
+       # img = loadmat(img_path)["B"]
+        img = np.load(img_path)
         label = img_path.split("/")[-1].split("_")[0]   # 我将每个.mat文件名的GS当做类别label（你可以自行修改你原本的label)
-        sample = {"image": img, "label": label}
+        
+        labels = torch.from_numpy(np.array(class_name_dict[label]))
+        images = torch.from_numpy(np.array(img).transpose(2, 0, 1)).float()
 
         if self.transform:
-            sample = self.transform(sample)
-        return sample
+            sample = self.transform(images)
+        return images, labels
 
 
 if __name__ == '__main__':
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print("using {} device.".format(device))
-        class_name_dict = {"GS": 0, "MG": 1, "NX": 2, "QH": 3, "XJ": 4}
-        data = GOU_QI_DATA("/bak4t/back8t/v1/yangdataset/large_dataset/GS/new_data/", transform=None)
-        # data = GOU_QI_DATA('./new_data/', transform=None)
 
-        dataloader = DataLoader(data, batch_size=64, num_workers=4, shuffle=True)
+       # data = GOU_QI_DATA("/bak4t/back8t/v1/yangdataset/large_dataset/GS/new_data/", transform=None)
+        # data = GOU_QI_DATA('./new_data/', transform=None)
+        train_data = GOU_QI_DATA("/bak4t/back8t/v1/yangdataset/large_dataset/selected_channel_datasets/",transform=None)
+        train_loader = DataLoader(train_data, batch_size=64, num_workers=4, shuffle=True)
+       
+        test_data = GOU_QI_DATA("/bak4t/back8t/v1/yangdataset/large_dataset/selected_channel_test/", transform=None)
+        test_loader = DataLoader(test_data, batch_size=64, num_workers=4, shuffle=False)
+
         #for i_batch, batch_data in enumerate(dataloader):
         #    print(i_batch)  # 打印batch编号
         #    print(batch_data['image'].size())  # 打印该batch里面图片的大小
         #    print(batch_data['label'])  # 打印该batch里面图片的标签
         print("The data is loaded ! ")
-        # model_weight_path = "./resNet34.pth"
-        net = resnet34(num_classes=1, inputchannel=728)
-        # assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
-        # net.load_state_dict(torch.load(model_weight_path, map_location=device))
-        # net.cuda()
+        model_weight_path = "./resNet34.pth"
+        net = resnet34(num_classes=1000, inputchannel=3)
+        assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+        net.load_state_dict(torch.load(model_weight_path, map_location=device))
+        net.cuda()
         # # change fc layer structure
         in_channel = net.fc.in_features
         net.fc = nn.Linear(in_channel, 5)
-        net.conv1.in_channels = 728
+        net.conv1.in_channels = 35
         net.to(device)  # move to device
         print(net)
         print("The structure of net is ok ! ")
@@ -70,17 +79,16 @@ if __name__ == '__main__':
         epochs = 50
         best_acc = 0.0
         save_path = './resNet34-final.pth'
-        train_steps = len(dataloader)
+        train_steps = len(train_loader)
         for epoch in range(epochs):
             # train
             net.train()
             running_loss = 0.0
-            train_bar = tqdm(dataloader)
+            train_bar = tqdm(train_loader)
             for step, data in enumerate(train_bar):
-                # images, labels = data
-                labels = torch.from_numpy(np.array(class_name_dict[data["label"][0]])).cuda()
-                images = torch.from_numpy(np.array(data["image"]).transpose(0, 3, 1, 2)).cuda()
-                images = images.float()
+                images, labels = data
+                labels = labels.cuda()
+                images = images.cuda()
                 optimizer.zero_grad()
                 logits = net(images.to(device))
                 print(labels.size(),labels.type())
@@ -110,7 +118,7 @@ if __name__ == '__main__':
                     val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
                                                                epochs)
 
-            val_accurate = acc / len_test
+            val_accurate = acc / len(test_loader)
             print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
                   (epoch + 1, running_loss / train_steps, val_accurate))
 
