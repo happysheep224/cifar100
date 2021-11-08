007943f441585c27fedaf22f00a548a693e15c3c
happysheep224 <89255775@qq.com>
2021-11-08 14:12:28 +0800

    macpro

--- Test5_resnet/train_c10.py
+++ Test5_resnet/train_c10.py
@@ -58,7 +58,7 @@ def main():
         break
 
     model_weight_path = "./resNet34.pth"
-    net = resnet34()
+    net = resnet34(num_classes=1000)
     assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
     net.load_state_dict(torch.load(model_weight_path, map_location=device))
     # net.cuda()
